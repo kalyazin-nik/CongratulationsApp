a34191f3a3a9b9c6367e97667019824c229c1235
Nikita <kalyazin.nik@yandex.ru>
2024-07-31 00:21:59 +0300

    added DbContextFactory and appsettings.json

---
+++ Congratulations/Factories/DbContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using Congratulations.Database;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Congratulations.Factories;
+
+internal class DbContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    private static DbContextOptions<DataContext> _options = null!;
+    internal static DbContextOptions<DataContext> Options => _options ?? GetOptions();
+
+    private static DbContextOptions<DataContext> GetOptions()
+    {
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        builder.AddJsonFile("appsettings.json");
+        var config = builder.Build();
+        var connectionString = config.GetConnectionString("PostgresConnection");
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        _options = optionsBuilder.UseNpgsql(connectionString).Options;
+
+        return _options;
+    }
+
+    public DataContext CreateDbContext(string[] args)
+    {
+        return new DataContext(Options);
+    }
+
+    internal static DataContext CreateDataContext()
+    {
+        return new DataContext(Options);
+    }
+}
