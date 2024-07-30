130f18372d2f991ec6db9c532b53ddf068bbd3c5
Nikita <kalyazin.nik@yandex.ru>
2024-07-30 22:32:36 +0300

    Добавьте файлы проекта.

---
+++ Congratulations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Congratulations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
