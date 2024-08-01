b126bf04994adda015b65447c905469871fc7cc9
Nikita <kalyazin.nik@yandex.ru>
2024-08-02 01:05:13 +0300

    first migration in database

--- Congratulations.Database/DataContext.cs
+++ Congratulations.Database/DataContext.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Congratulations.Entities;
+using Congratulations.Entities.Settings.Builders;
+using Microsoft.EntityFrameworkCore;
 
 namespace Congratulations.Database;
 
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
+    public DbSet<Employee> Employees { get; set; } = null!;
+    public DbSet<Address> Addresses { get; set; } = null!;
+    public DbSet<Phone> Phones { get; set; } = null!;
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        EntityBuilder<Employee>.ChangeEntity(modelBuilder);
+        EntityBuilder<Address>.ChangeEntity(modelBuilder);
+        EntityBuilder<Phone>.ChangeEntity(modelBuilder);
     }
 }
